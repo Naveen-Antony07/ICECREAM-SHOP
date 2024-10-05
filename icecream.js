4e2f6391d94cb7d3dcd76978fdaceab7439b53a7
Naveen Antony <147585976+Naveen-Antony07@users.noreply.github.com>
2024-10-05 08:36:19 +0530

    Add files via upload
    You are managing the inventory for an ice cream shop. Each type of ice cream is represented as an object with the following properties:
    
     A. flavor
     B. Price
     C. Quantity

---
+++ icecream.js
@@ -0,0 +1,7 @@
+const flavour={1:"vannila",2:"chocolate",3:"strawberry",4:"blackcurrent"};
+const prize={1:50,2:70,3:60,4:80};
+flavour[5]="butterscotch"
+flavour[1]="mango"
+console.log(flavour)
+const icecream_count=Object.keys(flavour).length;
+console.log(icecream_count)
